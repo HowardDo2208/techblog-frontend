07b00cd0b9be960b55f527598c7e2839ebc3f8c4
Howard Do <howarddo2208@gmail.com>
2022-04-18 10:57:00 +0700

    basic authentication

--- config/frontendConfig.ts
+++ config/frontendConfig.ts
@@ -1,4 +1,6 @@
-import ThirdPartyEmailPasswordReact from 'supertokens-auth-react/recipe/thirdpartyemailpassword'
+import ThirdPartyEmailPasswordReact, {
+  Google,
+} from 'supertokens-auth-react/recipe/thirdpartyemailpassword'
 import SessionReact from 'supertokens-auth-react/recipe/session'
 import { appInfo } from './appInfo'
 
@@ -8,15 +10,21 @@ export const frontendConfig = () => {
     recipeList: [
       ThirdPartyEmailPasswordReact.init({
         signInAndUpFeature: {
-          providers: [
-            ThirdPartyEmailPasswordReact.Google.init(),
-            ThirdPartyEmailPasswordReact.Facebook.init(),
-            ThirdPartyEmailPasswordReact.Github.init(),
-            ThirdPartyEmailPasswordReact.Apple.init(),
-          ],
+          providers: [Google.init()],
+        },
+        getRedirectionURL: async (context) => {
+          if (context.action === 'SUCCESS') {
+            if (context.redirectToPath !== undefined) {
+              // we are navigating back to where the user was before they authenticated
+              return context.redirectToPath
+            }
+            return '/'
+          }
+          return undefined
         },
       }),
       SessionReact.init(),
     ],
   }
 }
+
