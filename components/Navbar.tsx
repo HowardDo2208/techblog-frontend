82f151ff56c028ef3607fd80e73224ffa138409c
Howard Do <howarddo2208@gmail.com>
2022-04-18 13:02:31 +0700

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,21 @@
-import React from "react";
-import { signOut } from "supertokens-auth-react/recipe/thirdpartyemailpassword";
+import React from 'react'
+import { signOut } from 'supertokens-auth-react/recipe/thirdpartyemailpassword'
 
 export function NavBar() {
   async function onLogout() {
-    await signOut();
-    window.location.href = "/";
+    await signOut()
+    window.location.href = '/'
+  }
+
+  function onSigninup() {
+    window.location.href = '/auth'
   }
   return (
     <ul>
       <li>Home</li>
       <li onClick={onLogout}>Logout</li>
-
+      <li onClick={onSigninup}>Signinup</li>
     </ul>
   )
 }
+
