cbb551bd45cf5ad088a93c04f31d0d76b2b35305
Howard Do <howarddo2208@gmail.com>
2022-04-18 13:05:38 +0700

    final changel

--- config/appInfo.ts
+++ config/appInfo.ts
@@ -1,7 +1,9 @@
 export const appInfo = {
   appName: 'techblog',
   apiDomain: process.env.API_DOMAIN || 'http://localhost:3000',
-  websiteDomain: process.env.WEBSITE_DOMAIN || 'http://localhost:3080',
+  websiteDomain:
+    process.env.WEBSITE_DOMAIN || 'https://techblog-frontend.vercel.app',
   apiBasePath: '/auth',
   websiteBasePath: '/auth',
 }
+
