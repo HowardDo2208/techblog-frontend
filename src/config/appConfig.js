539cc653d68fea892a43651ddcf44ae8ec87b9b0
Howard Do <howarddo2208@gmail.com>
2022-05-10 00:52:47 +0700

    initialize supertokens

---
+++ src/config/appConfig.js
@@ -0,0 +1,8 @@
+export const appInfo = {
+  appName: 'techblog',
+  apiDomain: process.env.NEXT_PUBLIC_API_DOMAIN || 'http://localhost:3000',
+  websiteDomain:
+    process.env.NEXT_PUBLIC_WEBSITE_DOMAIN || 'http://localhost:3080',
+  apiBasePath: '/auth',
+  websiteBasePath: '/auth',
+}
