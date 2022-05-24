a31689bff45f7293c997cd1813fccc60658c0b80
Howard Do <howarddo2208@gmail.com>
2022-05-24 10:01:41 +0700

    fix auth config

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,7 +1,7 @@
 export const appInfo = {
   appName: 'techblog',
-  apiDomain: process.env.NEXT_PUBLIC_API_DOMAIN || 'http://localhost:3000',
-  websiteDomain: process.env.NEXT_PUBLIC_WEBSITE_DOMAIN || 'http://localhost:3080',
+  apiDomain: process.env.PUBLIC_API_DOMAIN || 'http://localhost:3000',
+  websiteDomain: process.env.PUBLIC_API_DOMAIN || 'http://localhost:3080',
   apiBasePath: '/auth',
   websiteBasePath: '/auth'
-};
+}
