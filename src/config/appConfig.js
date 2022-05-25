da1ef7be7340b7bdbf38070c0e06aa98ed5468c6
Howard Do <howarddo2208@gmail.com>
2022-05-25 18:42:51 +0700

    fix env

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,7 +1,7 @@
 export const appInfo = {
   appName: 'techblog',
   apiDomain: process.env.REACT_APP_PUBLIC_API_DOMAIN,
-  websiteDomain: process.env.REACT_APP_PUBLIC_API_DOMAIN,
+  websiteDomain: process.env.REACT_APP_PUBLIC_WEBSITE_DOMAIN,
   apiBasePath: '/auth',
   websiteBasePath: '/auth'
 }
